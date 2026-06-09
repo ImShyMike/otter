953efb581771f0e2bf7dd97705036459fe1744bd
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2026-06-09 14:39:01 +0100

    fix: serve opensearch.xml with correct headers

--- frontend/src/routes/opensearch.xml/+server.ts
+++ frontend/src/routes/opensearch.xml/+server.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export function GET() {
+	return new Response(
+		`<?xml version="1.0" encoding="UTF-8"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
     <ShortName>Otter</ShortName>
     <LongName>Otter - Hack Club Projects Search</LongName>
@@ -9,4 +11,11 @@
     <Language>en-us</Language>
     <OutputEncoding>UTF-8</OutputEncoding>
     <InputEncoding>UTF-8</InputEncoding>
-</OpenSearchDescription>
+</OpenSearchDescription>`,
+		{
+			headers: {
+				'Content-Type': 'application/opensearchdescription+xml'
+			}
+		}
+	);
+}
