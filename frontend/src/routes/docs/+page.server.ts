b9e9337dd8585264cc005f40080f194c9fa71aef
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2026-06-18 12:33:32 +0100

    feat: add docs redirect

---
+++ frontend/src/routes/docs/+page.server.ts
@@ -0,0 +1,7 @@
+import { redirect } from '@sveltejs/kit';
+import { env } from '$env/dynamic/public';
+
+export function load() {
+    const api = env.PUBLIC_API_BASE || 'http://localhost:3000';
+	redirect(307, `${api}/docs`);
+}
